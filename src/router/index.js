571e513b3a0039db16adb8ce223e664091e4650f
Lucia <lucia.wu02@sap.com>
2020-03-27 13:18:20 +0800

    remove

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,4 @@
 import Home from '@/components/Home'
-import school from "@/screen/school/list.vue";
-import schoolDetail from "@/screen/school/detail.vue";
-export default [ 
-{ path: '/Home', component: Home },
-{ path: '/school', component: school },
-{ path: '/schoolDetail', component: schoolDetail },
+export default [
+    { path: '/Home', component: Home },
 ]
