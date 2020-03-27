30b6878b57f9244c66ce508439cfa7b2032ad06a
Lucia <lucia.wu02@sap.com>
2020-03-27 13:52:13 +0800

    menu

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
         <el-main>
           <el-breadcrumb separator-class="el-icon-arrow-right">
             <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>当前项目</el-breadcrumb-item>
           </el-breadcrumb>
           <router-view />
         </el-main>
